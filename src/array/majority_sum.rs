0b660f3d139895b76ed2b8729f8cca4a96971b19
DreamYao <yaojun@g7.com.cn>
2024-01-23 20:14:09 +0800

    算法归类

--- src/array/majority_sum.rs
+++ src/array/majority_sum.rs
@@ -1,4 +1,221 @@
 use std::collections::HashMap;
+
+/// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+/// 
+/// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+/// 
+/// 你可以按任意顺序返回答案。
+/// 
+/// 示例 1：
+/// 
+/// 输入：nums = [2,7,11,15], target = 9
+/// 输出：[0,1]
+/// 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+/// 
+/// 示例 2：
+/// 
+/// 输入：nums = [3,2,4], target = 6
+/// 输出：[1,2]
+/// 
+/// 示例 3：
+/// 
+/// 输入：nums = [3,3], target = 6
+/// 输出：[0,1]
+/// 
+/// 提示：
+/// 
+/// 2 <= nums.length <= 104
+/// -109 <= nums[i] <= 109
+/// -109 <= target <= 109
+/// 
+/// 只会存在一个有效答案
+/// 
+/// 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map = HashMap::with_capacity(nums.len());
+    for (index, val) in nums.iter().enumerate() {
+        match map.get(&(target - val)) {
+            // i为原本map中的数据索引，index为新加入的数据索引
+            Some(&i) => return vec![i as i32, index as i32],
+            // 不存在则把数据的值作为key，索引作为value存入map
+            None => map.insert(val, index),
+        };
+    }
+    // 返回空数组
+    vec![]
+}
+
+/// 三数之和
+/// 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a ，b ，c ，使得 a + b + c = 0 ？请找出所有和为 0 且 不重复 的三元组。
+///
+/// 示例 1：
+///
+/// 输入：nums = [-1,0,1,2,-1,-4]
+/// 输出：[[-1,-1,2],[-1,0,1]]
+/// 示例 2：
+///
+/// 输入：nums = []
+/// 输出：[]
+/// 示例 3：
+///
+/// 输入：nums = [0]
+/// 输出：[]
+///
+/// 提示：
+///
+/// 0 <= nums.length <= 3000
+/// -105 <= nums[i] <= 105
+/// 解题思路：固定i元素，在通过头和尾双指针从头尾向中间遍历，找到所有符合条件的元素
+pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut nums = nums;
+    // 数组长度
+    let len = nums.len();
+    let mut res = vec![];
+    if len < 3 {
+        return res;
+    }
+    // 先对数组进行排序
+    nums.sort();
+    
+    for i in 0..len - 2 {
+        if i > 0 && nums[i] == nums[i - 1] {
+            // 去重
+            continue;
+        }
+
+        let s = nums[i] + nums[i + 1] + nums[i + 2];
+        if s > 0 {
+            // 由于数组已经排序，后面无论怎么选，选出的三个数的和不会比 s 还小，所以不会找到比 s 更优的答案了。
+            break;
+        }
+        let s = nums[i] + nums[len - 2] + nums[len - 1];
+        if s < 0 {
+            // 由于数组已经排序，nums[i] 加上后面任意两个数都不超过 s，所以下面的双指针就不需要跑了，只需要继续取下一个数即可。
+            continue;
+        }
+
+        // 固定i，寻找first和last，使用双指针法
+        let mut first = i + 1;
+        // 尾指针
+        let mut last = len - 1;
+        while first < last {
+            if nums[first] + nums[last] + nums[i] < 0 {
+                // 小于0，first右移
+                first += 1;
+            } else if nums[first] + nums[last] + nums[i] > 0 {
+                // 大于0，last左移
+                last -= 1;
+            } else {
+                // 等于0，加入结果集
+                res.push(vec![nums[i], nums[first], nums[last]]);
+                // first和last去重
+                while first < last && nums[first] == nums[first + 1] {
+                    first += 1;
+                }
+                while first < last && nums[last] == nums[last - 1] {
+                    last -= 1;
+                }
+
+                // first和last继续移动
+                first += 1;
+                last -= 1;
+            }
+        }
+    }
+
+    res
+}
+
+/// 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+///
+/// 返回这三个数的和。
+///
+/// 假定每组输入只存在恰好一个解。
+///
+/// 示例 1：
+///
+/// 输入：nums = [-1,2,1,-4], target = 1
+/// 输出：2
+/// 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+/// 示例 2：
+///
+/// 输入：nums = [0,0,0], target = 1
+/// 输出：0
+///
+/// 提示：
+///
+/// 3 <= nums.length <= 1000
+/// -1000 <= nums[i] <= 1000
+/// -104 <= target <= 104
+/// 有两个优化可以让代码击败接近 100%！（和三数之和一样）
+///
+/// 设 s = nums[i] + nums[i+1] + nums[i+2]。如果 s > target，由于数组已经排序，后面无论怎么选，
+/// 选出的三个数的和不会比 s 还小，所以不会找到比 s 更优的答案了。所以只要 s > target，就可以直接 break 外层循环了。
+/// 在 break 前判断 s 是否离 target 更近，如果更近，那么更新答案为 s。
+///
+/// 设 s = nums[i] + nums[n-2] + nums[n-1]。如果 s < target，由于数组已经排序，
+/// nums[i] 加上后面任意两个数都不超过 s，所以下面的双指针就不需要跑了，无法找到比 s 更优的答案。
+/// 但是后面还有更大的 nums[i]，可能找到一个离 target 更近的三数之和，所以还需要继续枚举，continue 外层循环。
+/// 在 continue 前判断 s 是否离 target 更近，如果更近，那么更新答案为 s。
+pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
+    let mut nums = nums;
+    let len = nums.len();
+    // 三数之和
+    let mut ans = 0;
+    // 最小差值
+    let mut min_diff = i32::MAX;
+    // 先对数组进行排序
+    nums.sort();
+    for i in 0..len - 2 {
+        if i > 0 && nums[i] == nums[i - 1] {
+            // 去重
+            continue;
+        }
+        let count = nums[i] + nums[i + 1] + nums[i + 2];
+        if count > target {
+            // 由于数组已经排序，后面无论怎么选，选出的三个数的和不会比 s 还小，所以不会找到比 s 更优的答案了。
+            // 所以只要 s > target，就可以直接 break 外层循环了。
+            if count - target < min_diff {
+                ans = count;
+            }
+            break;
+        }
+        let count = nums[i] + nums[len - 2] + nums[len - 1];
+        if count < target {
+            // 由于数组已经排序，nums[i] 加上后面任意两个数都不超过 s，所以下面的双指针就不需要跑了，
+            // 无法找到比 s 更优的答案。但是后面还有更大的 nums[i]，可能找到一个离 target 更近的三数之和，
+            // 所以还需要继续枚举，continue 外层循环。
+            if target - count < min_diff {
+                min_diff = target - count;
+                ans = count;
+            }
+            continue;
+        }
+
+        // 头指针
+        let mut left = i + 1;
+        // 尾指针
+        let mut right = len - 1;
+        while left < right {
+            let count = nums[i] + nums[left] + nums[right];
+            if count == target {
+                return count;
+            } else if count < target {
+                // 小于target，left右移
+                left += 1;
+            } else {
+                // 大于target，right左移
+                right -= 1;
+            }
+            if (target - count).abs() < min_diff {
+                min_diff = (target - count).abs();
+                ans = count;
+            }
+        }
+    }
+    ans
+}
+
 /// 四数之和
 /// 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
 ///
@@ -165,6 +382,29 @@ pub fn four_sum_count(nums1: Vec<i32>, nums2: Vec<i32>, nums3: Vec<i32>, nums4: 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_two_sum() {
+        let nums = vec![2, 7, 11, 15];
+        let target = 9;
+        let two_sum = two_sum(nums, target);
+        println!("{:?}", two_sum)
+    }
+
+    #[test]
+    fn test_three_sum() {
+        let nums = vec![-1, 0, 1, 2, -1, -4];
+        let res = three_sum(nums);
+        assert_eq!(res, vec![vec![-1, -1, 2], vec![-1, 0, 1]]);
+    }
+
+    #[test]
+    fn test_three_sum_closest() {
+        let nums = vec![-1, 2, 1, -4];
+        let target = 1;
+        let res = three_sum_closest(nums, target);
+        assert_eq!(res, 2);
+    }
 
     #[test]
     fn test_four_sum() {
--- src/array/prefix_sum.rs
+++ src/array/prefix_sum.rs
@@ -77,37 +77,6 @@ pub fn max_sub_array(nums: Vec<i32>) -> i32 {
     res
 }
 
-/// https://leetcode.cn/problems/maximum-subarray/solutions/2361770/53-zui-da-zi-shu-zu-he-dong-tai-gui-hua-bvkq9/
-/// 解题思路：
-/// 动态规划是本题的最优解法，以下按照标准流程解题。
-///
-/// 常见解法	时间复杂度	空间复杂度
-/// 暴力搜索	O(N^2)     O(1)
-/// 分治思想	O(Nlog⁡N)	O(log⁡N)
-/// 动态规划	O(N)	   O(1)
-/// 动态规划解析：
-///
-/// 状态定义： 设动态规划列表 dp ，dp[i] 代表以元素 nums[i] 为结尾的连续子数组最大和。
-///
-/// 为何定义最大和 dp[i] 中必须包含元素 nums[i] ：保证 dp[i] 递推到 dp[i+1] 的正确性；如果不包含 nums[i]，递推时则不满足题目的 连续子数组 要求。
-/// 转移方程： 若 dp[i−1]≤0 ，说明 dp[i−1] 对 dp[i]产生负贡献，即 dp[i−1]+nums[i] 还不如 nums[i] 本身大。
- ///
-/// 初始状态： dp[0]=nums[0]，即以 nums[0] 结尾的连续子数组最大和为 nums[0] 。
-///
-/// 返回值： 返回 dp 列表中的最大值，代表全局最大值。
-/// 状态压缩：
-/// 由于 dp[i] 只与 dp[i−1] 和 nums[i] 有关系，因此可以将原数组 nums 用作 dp 列表，即直接在 nums 上修改即可。
-/// 由于省去 dp 列表使用的额外空间，因此空间复杂度从 O(N) 降至 O(1) 。
-/// <img class="marble" src="https://pic.leetcode-cn.com/77d1aa6a444743d3c8606ac951cd7fc38faf68a62064fd2639df517cd666a4d0-Picture1.png" alt="">
-pub fn max_sub_array_dym(nums: Vec<i32>) -> i32 {
-    let mut nums = nums;
-    let mut res = nums[0];
-    for i in 1..nums.len() {
-        nums[i] += max(nums[i - 1], 0);
-        res = max(res, nums[i]);
-    }
-    res
-}
 
 #[cfg(test)]
 mod tests {
@@ -117,11 +86,5 @@ mod tests {
     fn test_max_sub_array() {
         let nums = vec![-2, 1, -3, 4, -1, 2, 1, -5, 4];
         assert_eq!(max_sub_array(nums), 6);
-    }
-
-    #[test]
-    fn test_max_sub_array_dym() {
-        let nums = vec![-2, 1, -3, 4, -1, 2, 1, -5, 4];
-        assert_eq!(max_sub_array_dym(nums), 6);
     }
 }
