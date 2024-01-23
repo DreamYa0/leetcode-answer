2fd7375ad100d55498fba75440c9c313a0d47e4c
DreamYao <yaojun@g7.com.cn>
2024-01-23 23:15:08 +0800

    双指针法

--- src/array/two_pointers.rs
+++ src/array/two_pointers.rs
@@ -267,6 +267,46 @@ pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
     res
 }
 
+/// 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+/// 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+/// 例如，121 是回文，而 123 不是。
+///
+/// 示例 1：
+/// 输入：x = 121
+/// 输出：true
+///
+/// 示例 2：
+/// 输入：x = -121
+/// 输出：false
+/// 解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+///
+/// 示例 3：
+/// 输入：x = 10
+/// 输出：false
+/// 解释：从右向左读, 为 01 。因此它不是一个回文数。
+///
+/// 提示：
+/// -231 <= x <= 231 - 1
+/// 进阶：你能不将整数转为字符串来解决这个问题吗？
+/// 解题思路：使用双指针法
+pub fn is_palindrome(x: i32) -> bool {
+    let mut bys = x.to_string().into_bytes();
+    // 定义右指针
+    let mut right = bys.len() - 1;
+    // 左右指针往中间 bys.len() / 2 位置移动
+    for left in 0..bys.len() / 2 {
+        // 交换左右指针的值
+        let temp = bys[left];
+        bys[left] = bys[right];
+        bys[right] = temp;
+        // 右指针左移
+        right -= 1;
+    }
+    // 将字节数组转换为字符串
+    let s = String::from_utf8(bys).unwrap();
+    s == x.to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -299,5 +339,10 @@ mod tests {
         let nums = vec![-4, -1, 0, 3, 10];
         let res = sorted_squares(nums);
         println!("{:?}", res);
+    }
+
+    #[test]
+    fn test_is_palindrome() {
+        println!("{:?}", is_palindrome(-121))
     }
 }
